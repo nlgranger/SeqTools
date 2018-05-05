9ca0a804c6e7b064189caed9585bb7b0a310de10
Nicolas Granger <nicolas.granger.m@gmail.com>
2018-05-05 14:56:17 +0200

    Finalizing release 0.8.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     license="MPL2",
     url="https://github.com/nlgranger/SeqTools",
     packages=['seqtools'],
-    version='0.8.dev',
+    version='0.8',
     install_requires=[
         'typing;python_version<"3.6"',
         'backports.weakref;python_version<"3"',
